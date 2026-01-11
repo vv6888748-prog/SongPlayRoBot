9acb21d0381c7f34667cb19ae47d6c6a0e5057b3
vv6888748-prog <vv6888748@gmail.com>
2026-01-11 10:37:01 +0530

    Update config.py
    Hinata music bot tamil

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
-    API_ID = ""  # 🤖 Same As APP_ID. Get It From My.telegram.org.
-    API_HASH = "" # 🤖 Get It From My.telegram.org.
-    BOT_TOKEN = "" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.
+    API_ID = "34128025"  # 🤖 Same As APP_ID. Get It From My.telegram.org.
+    API_HASH = "ba8a75d7cd5cc38893381936c09e92fb" # 🤖 Get It From My.telegram.org.
+    BOT_TOKEN = "7271144036:AAGBfuXQuoAHXcTycWG-" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.
